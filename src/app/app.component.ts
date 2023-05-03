cb3290c59f498a610fa1838f217776396e778779
Albin Aj <albinattumalil@gmail.com>
2023-05-03 11:22:52 +0530

    page commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'material-wind';
+  product: icon[] = [
+    {
+      text: 'Dashboard',
+      icon: 'home',
+    },
+    { text: 'Custermers', icon: 'person' },
+    { text: 'Solutions', icon: 'school' },
+    { text: 'Settings', icon: 'settings' },
+  ];
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+originally bred for hunting.`;
+}
+interface icon {
+  text: string;
+  icon: string;
 }
